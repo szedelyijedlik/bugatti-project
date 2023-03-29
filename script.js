1e77c89d10f6b2dcd63e0e8cdc16d26cbfbd77a7
Szédelyi Ábris <szedelyi.abris.peter@students.jedlik.eu>
2023-03-29 17:14:31 +0200

    v0.11 Navbar fix

--- script.js
+++ script.js
@@ -8,15 +8,32 @@ window.onload = function () {
 
 function welcomeAnimation(){
     let navLogoImg = document.querySelector(".navbar-brand svg");
-    let navBar = document.querySelector(".navbar-nav");
+    let navBar = document.querySelector(".navbar-collapse");
     let floatDiv = document.querySelector(".floatDiv");
 
+    navLogoImg.style.transform = "none";
     navLogoImg.style.left = "70px";
-    navLogoImg.style.top = "-45px";
+    navLogoImg.style.top = "-40px";
     navLogoImg.style.width = "90px";
-    navBar.style.right = "100px"
+    navBar.style.transform = "none";
+    navBar.style.right = "-200px"
     navBar.style.top = "40px"
     floatDiv.style.width = "100%";
     floatDiv.style.height = "13vh";
     floatDiv.style.borderRadius = "0";
 }
+
+setTimeout(scrollToContinue, 10);
+
+function scrollToContinue(){
+    let scrollDiv = document.querySelector("a.scrollToContinue");
+    scrollDiv.style.opacity = 1;
+    setTimeout(scrollToContinueBack, 2000);
+}
+
+function scrollToContinueBack(){
+    let scrollDiv = document.querySelector("a.scrollToContinue");
+    scrollDiv.style.opacity = 0.1;
+    setTimeout(scrollToContinue, 2000);
+}
+
