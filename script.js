88a2ea07b4b3ca48c3a949c946c5f6db64ea99ff
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2023-03-31 08:00:17 +0200

    v0.211 Fix

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 window.onload = function () {
-    window.onscroll = function () {
-      if (document.body.scrollTop < 10) {
-        welcomeAnimation();
-      }
-    };
+  window.onscroll = function () {
+    if (document.body.scrollTop < 10) {
+      welcomeAnimation();
+      imgAnimation();
+    }
   };
+};
 
-function welcomeAnimation(){
-    let navLogoImg = document.querySelector(".navbar-brand");
-    let navBar = document.querySelector(".navbar-collapse");
-    let floatDiv = document.querySelector(".floatDiv");
-    let navbarToggler = document.querySelector(".navbar-toggler");
+function welcomeAnimation() {
+  let navLogoImg = document.querySelector(".navbar-brand");
+  let navBar = document.querySelector(".navbar-collapse");
+  let floatDiv = document.querySelector(".floatDiv");
+  let navbarToggler = document.querySelector(".navbar-toggler");
 
-    navLogoImg.classList.add("manCollapse");
-    navBar.classList.add("manCollapse");
-    floatDiv.classList.add("manCollapse");
-    navbarToggler.classList.add("manCollapse");
+  navLogoImg.classList.add("manCollapse");
+  navBar.classList.add("manCollapse");
+  floatDiv.classList.add("manCollapse");
+  navbarToggler.classList.add("manCollapse");
 }
 
 setTimeout(scrollToContinue, 10);
 
-function scrollToContinue(){
-    let scrollDiv = document.querySelector("a.scrollToContinue");
-    scrollDiv.style.opacity = 1;
-    setTimeout(scrollToContinueBack, 2000);
+function scrollToContinue() {
+  let scrollDiv = document.querySelector("a.scrollToContinue");
+  scrollDiv.style.opacity = 1;
+  setTimeout(scrollToContinueBack, 2000);
 }
 
-function scrollToContinueBack(){
-    let scrollDiv = document.querySelector("a.scrollToContinue");
-    scrollDiv.style.opacity = 0.1;
-    setTimeout(scrollToContinue, 2000);
+function scrollToContinueBack() {
+  let scrollDiv = document.querySelector("a.scrollToContinue");
+  scrollDiv.style.opacity = 0.1;
+  setTimeout(scrollToContinue, 2000);
 }
 
 let navbarCollapse = document.querySelector(".navbar-collapse");
